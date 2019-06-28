aa75cb7524bb1b63d37798cca96b3786ceb2138e
danswick <dan.swick@gmail.com>
2019-06-28 16:14:42 -0700

    no basepath

--- underreact.config.js
+++ underreact.config.js
@@ -4,7 +4,6 @@ const html = require('./html');
 const path = require('path');
 
 module.exports = {
-  siteBasePath: '/map-template',
   outputDirectory: path.join(__dirname, '/docs'),
   htmlSource: html()
 };
